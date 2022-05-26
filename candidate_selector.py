8951a676f0eb8cb12f3943d5b5276e7554e5981d
Malte Appel <malte@iij-ii.co.jp>
2022-05-26 03:27:53 +0000

    Change print to logging and add calculate_scores function that can be used from a different module

--- candidate_selector.py
+++ candidate_selector.py
@@ -1,5 +1,6 @@
 import argparse
 import bz2
+import logging
 import pickle
 import sys
 from collections import Counter
@@ -11,23 +12,23 @@ OUTPUT_DELIMITER = ','
 
 
 def read_input(input_file: str) -> dict:
-    print(f'Reading data from: {input_file}')
+    logging.info(f'Reading data from: {input_file}')
     with bz2.open(input_file, 'r') as f:
         ret = pickle.load(f)
     if not isinstance(ret, dict):
-        print(f'Error: Expected pickled dict, got: {type(ret)}',
+        logging.info(f'Error: Expected pickled dict, got: {type(ret)}',
               file=sys.stderr)
         return dict()
-    print(f'Read {len(ret)} entries')
+    logging.info(f'Read {len(ret)} entries')
     l1_key = list(ret.keys())[0]
     l2_key = list(ret[l1_key].keys())[0]
     if type(ret[l1_key][l2_key]) != int:
-        print(f'Casting data to int')
+        logging.info(f'Casting data to int')
         ret = {l1: {l2: int(np.ceil(ret[l1][l2]))
                     for l2 in ret[l1]}
                for l1 in ret}
     if 'as_hops' in input_file:
-        print('AS hops detected, increasing by 1 hop to produce AS-path '
+        logging.info('AS hops detected, increasing by 1 hop to produce AS-path '
               'length')
         ret = {l1: {l2: int(ret[l1][l2] + 1)
                     for l2 in ret[l1]}
@@ -36,25 +37,25 @@ def read_input(input_file: str) -> dict:
 
 
 def read_as_prefixes(input_file: str) -> dict:
-    print(f'Reading AS prefix counts from: {input_file}')
+    logging.info(f'Reading AS prefix counts from: {input_file}')
     ret = dict()
     with open(input_file, 'r') as f:
         f.readline()
         for line in f:
             asn, prefix_count = line.strip().split(',')
             ret[int(asn)] = int(prefix_count)
-    print(f'Read {len(ret)} prefix counts')
+    logging.info(f'Read {len(ret)} prefix counts')
     return ret
 
 
 def read_as_names(input_file: str) -> dict:
-    print(f'Reading AS names from: {input_file}')
+    logging.info(f'Reading AS names from: {input_file}')
     ret = dict()
     with open(input_file, 'r') as f:
         for line in f:
             asn, name = line.strip().split(maxsplit=1)
             ret[int(asn)] = name
-    print(f'Read {len(ret)} AS names')
+    logging.info(f'Read {len(ret)} AS names')
     return ret
 
 
@@ -70,13 +71,22 @@ def weighted_dist(samples: dict) -> float:
 
 def filter_data(data: dict, min_samples: int) -> dict:
     len_pre = len(data)
-    print(f'Removing entries with less than {min_samples} samples')
+    logging.info(f'Removing entries with less than {min_samples} samples')
     data = {asn: samples
             for asn, samples in data.items()
             if len(samples) >= min_samples}
     len_post = len(data)
-    print(f'Removed {len_pre - len_post} entries. Remaining: {len_post}')
+    logging.info(f'Removed {len_pre - len_post} entries. Remaining: {len_post}')
     return data
+
+
+def calculate_scores(data: dict, min_samples: int) -> list:
+    if min_samples > 0:
+        data = filter_data(data, min_samples)
+
+    scores = [(asn, weighted_dist(samples)) for asn, samples in data.items()]
+    scores.sort(key=lambda t: t[1])
+    return scores
 
 
 def main() -> None:
@@ -96,6 +106,14 @@ def main() -> None:
                              'top candidates')
     args = parser.parse_args()
 
+    log_fmt = '%(asctime)s %(levelname)s %(message)s'
+    logging.basicConfig(
+        format=log_fmt,
+        level=logging.INFO,
+        filename='candidate_selector.log',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
     as_names_file = args.as_names
     as_names = dict()
     if as_names_file:
@@ -107,12 +125,10 @@ def main() -> None:
     input_file = args.input_file
     data = read_input(input_file)
     min_samples = args.min_samples
-    if min_samples > 0:
-        data = filter_data(data, min_samples)
 
-    scores = [(asn, weighted_dist(samples)) for asn, samples in data.items()]
-    scores.sort(key=lambda t: t[1])
-    print('10 BEST CANDIDATES')
+    scores = calculate_scores(data, min_samples)
+
+    logging.info('10 BEST CANDIDATES')
     for asn, score in scores[:10]:
         samples = list(data[asn].values())
         prefix_count = None
@@ -123,17 +139,15 @@ def main() -> None:
         avg_sample = np.mean(samples)
         median_sample = np.median(samples)
         max_sample = max(samples)
-        print(f'AS{asn}')
-        print(f'Score: {score}')
-        print(f'Prefixes: {prefix_count}')
-        print(f'Samples: {sample_count} ({min_sample} / {avg_sample:.2f} / '
-              f'{median_sample} / {max_sample})')
-        print(f'Name: ', end='')
+        logging.info(f'AS{asn}')
+        logging.info(f'Score: {score}')
+        logging.info(f'Prefixes: {prefix_count}')
+        logging.info(f'Samples: {sample_count} ({min_sample} / '
+                     f'{avg_sample:.2f} / {median_sample} / {max_sample})')
         if asn in as_names:
-            print(as_names[asn], end='')
-        print('\n')
+            logging.info(f'Name: {as_names[asn]}')
 
-    print('10 WORST CANDIDATES')
+    logging.info('10 WORST CANDIDATES')
     for asn, score in scores[-10:]:
         samples = list(data[asn].values())
         prefix_count = None
@@ -144,18 +158,16 @@ def main() -> None:
         avg_sample = np.mean(samples)
         median_sample = np.median(samples)
         max_sample = max(samples)
-        print(f'AS{asn}')
-        print(f'Score: {score}')
-        print(f'Prefixes: {prefix_count}')
-        print(f'Samples: {sample_count} ({min_sample} / {avg_sample:.2f} / '
-              f'{median_sample} / {max_sample})')
-        print(f'Name: ', end='')
+        logging.info(f'AS{asn}')
+        logging.info(f'Score: {score}')
+        logging.info(f'Prefixes: {prefix_count}')
+        logging.info(f'Samples: {sample_count} ({min_sample} / '
+                     f'{avg_sample:.2f} / {median_sample} / {max_sample})')
         if asn in as_names:
-            print(as_names[asn], end='')
-        print('\n')
+            logging.info(f'Name: {as_names[asn]}')
 
     output_file = args.output_file
-    print(f'Writing {len(scores)} candidates to file: {output_file}')
+    logging.info(f'Writing {len(scores)} candidates to file: {output_file}')
     with open(output_file, 'w') as f:
         headers = ('asn', 'score', 'prefix_count', 'sample_count',
                    'min_sample', 'avg_sample', 'median_sample', 'max_sample')
@@ -166,7 +178,7 @@ def main() -> None:
             if asn in as_prefix_counts:
                 prefix_count = as_prefix_counts[asn]
             else:
-                print(f'Warning: Found no prefix count for AS{asn}')
+                logging.info(f'Warning: Found no prefix count for AS{asn}')
             sample_count = len(samples)
             min_sample = min(samples)
             avg_sample = np.mean(samples)
